65113686f09ee58d29348707de47a06a6b5c6341
Omkar Dixit <Omkar1219@gmail.com>
2023-09-27 18:23:00 +0530

    added a file bash file

---
+++ file.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir sec_files && cd sec_files
+echo "username=anshuk6469" > file1
+echo "secure-password" > password.txt
+echo "apikey=d090b88716b80b6eb671c13bcd836f37" >file2
+echo "base64encodedsecret=SGVsbG8gd29ybGQK" > file3
